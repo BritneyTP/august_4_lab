b688cc621458ea8f5def51920a881432506ef039
Britt Dawn <brittdawndev@gmail.com>
2016-08-04 15:55:19 -0700

    javascript pseudocode and more html input forms

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// This would need more thought - but we decided to work on GitHub processes and be thorough there rather than JS logic.
+// I did learn that tasks take more time.
+
+function init() {
+  input = document.getElementById('input');
+}
+
+function convert_inches_centimeters() {
+  // if the input is inches - convert to cm
+  // if the input is centimeters - convert to inches
+}
